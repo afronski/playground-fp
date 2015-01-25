b16a61679ce3d7032492d67282a8f96e446dd258
Wojciech Gawronski <afronski@gmail.com>
2015-01-25 12:51:14 +0100

    Compojure - Address Book (Chapter 4).

--- clojure/address-book/project.clj
+++ clojure/address-book/project.clj
@@ -1,16 +1,29 @@
 (defproject address-book "0.1.0-SNAPSHOT"
   :description "Address book prepared by afronski (based on Jarrod's tutorial)."
   :url "https://github.com/afronski/playground-fp"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [midje "1.6.3"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-midje "3.1.3"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [postgresql/postgresql "9.3-1102.jdbc41"]
+                 [yesql "0.5.0-rc1"]
+                 [environ "1.0.0"]]
+
+  :plugins [[lein-ring "0.9.1"]
+            [lein-environ "1.0.0"]]
+
   :ring {:handler address-book.core.handler/app
          :init address-book.core.handler/init }
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+
+  :profiles {:test-local {:dependencies [[midje "1.6.3"]
+                                         [javax.servlet/servlet-api "2.5"]
+                                         [ring-mock "0.1.5"]]
+                          :plugins      [[lein-midje "3.1.3"]]}
+             :test-env-vars {}
+             :dev-env-vars {}
+
+             :test [:test-local :test-env-vars]
+             :dev [:dev-env-vars]})
---
+++ clojure/address-book/src/address_book/core/models/address_book_queries.sql
@@ -0,0 +1,20 @@
+-- name: all-contacts
+-- Selects all contacts
+SELECT id, name, phone, email FROM contacts;
+
+-- name: insert-contact<!
+-- Queries a single contact
+INSERT INTO contacts (name, phone, email) VALUES (:name, :phone, :email);
+
+-- name: drop-contacts-table!
+-- Drop the contacts table
+DROP TABLE contacts;
+
+-- name: create-contacts-table-if-not-exists!
+-- Create the contacts table if it does not exist
+CREATE TABLE IF NOT EXISTS contacts (
+  id serial PRIMARY KEY,
+  name VARCHAR (20) NOT NULL,
+  phone VARCHAR (14) NOT NULL,
+  email VARCHAR (25) NOT NULL
+);
